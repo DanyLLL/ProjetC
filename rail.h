9f8835732a251df54f59fe4c1947160820291ebf
DanyLLL <dany.lim.91210@gmail.com>
2025-01-01 17:16:19 +0100

    Update rail.h

--- rail.h
+++ rail.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include "chevalet.h"
-
 typedef struct {
 	int valeur;
 	struct Maillon* suivant;
 }Maillon;
 
-enum { MAX_RAIL = 8, Last = MAX_RAIL - 1};
+enum { MAX_RAIL = 8, Last = MAX_RAIL - 1 };
 
 enum { GAUCHE = 0, DROITE = 1 };
 
@@ -19,16 +17,8 @@ typedef struct {
 
 int initRail(Rail* r);
 
-
 void ajtRail(Rail* r, const char l, int sens);
-
 
 void affRail(Rail* r);
 
-void ajtMotRail1(Rail* r, const char* m, int sens);
-
-void ajtMotRail2(Rail* r, const char* m, int sens);
-
-void triRail(Rail* r);
-
-void triRail2(Rail* r);
+void ajtMotRail(Rail* r, const char* m, int sens);
