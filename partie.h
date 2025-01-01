d2522cb44a5295511e401a428ff37484694b5c0e
DanyLLL <dany.lim.91210@gmail.com>
2025-01-01 17:13:37 +0100

    Update partie.h

--- partie.h
+++ partie.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "pioche.h"
 #include "mainj.h"
+#include "rail.h"
+
 /**
 * @brief Lance toutes les fonctions permettant de lancer la partie.
 */
-void lancerPartie(Pioche* pioche, Main* J1, Main* J2);
+void lancerPartie(Pioche* pioche, Main* J1, Main* J2, Rail* rail);
 
+/**
+* @brief Déroule la partie après avoir décidé de l'ordre de jeu.
+*/
 void deroulePartie(Main* J1, Main* J2);
 
-void afficheSituationCouranteJeu(Main* J1, Main* J2);
+/**
+* @brief Affiche les mains des joueurs et l'état du rail.
+*/
+void afficheSituationCouranteJeu(Main* mainJ1, Main* mainJ2, Rail* rail);
+
