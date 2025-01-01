be2eb9aae17e3e24aa0128b4f6a27ba1ab3cbcad
DanyLLL <dany.lim.91210@gmail.com>
2025-01-01 17:11:14 +0100

    Update partie.c

--- partie.c
+++ partie.c
@@ -4,31 +4,24 @@
 #include "partie.h"
 #include "mainj.h"
 #include "mot.h"
-#include "mainj.h"
+#include "rail.h"
 
 #pragma warning(disable : 4996)
 
-void lancerPartie(Pioche* pioche, Main* J1, Main* J2) {
+void lancerPartie(Pioche* pioche, Main* J1, Main* J2,Rail* rail) {
 	preparePiocheJeu(pioche);
 	prepareMainsJeu(pioche, J1, J2);
-	deterOrdreJeu(J1, J2);
-	afficheSituationCouranteJeu(J1, J2);
+	deterOrdreJeu(J1, J2,rail);
+	afficheSituationCouranteJeu(J1, J2,rail);
 	deroulePartie(J1, J2);
 }
 
 void deroulePartie(Main* J1, Main* J2) {
-	if (deterOrdreJeu(J1, J2) == 0) {
-		printf("1> ");
-	}
-	else {
-		printf("2> ");
-	}
 }
 
-void afficheSituationCouranteJeu(Main* mainJ1,Main* mainJ2) {
-	printf("1 : %s\n", mainJ1->chevalets);
+void afficheSituationCouranteJeu(Main* mainJ1,Main* mainJ2,Rail* rail) {
+	printf("\n1 : %s\n", mainJ1->chevalets);
 	printf("2 : %s\n", mainJ2->chevalets);
-	printf("R : %s\n");
-	printf("V : %s\n\n");
+	affRail(rail);
 }
 
