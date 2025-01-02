791472cdf97be1883a8edcc1ef1aefd6a4db7071
DanyLLL <dany.lim.91210@gmail.com>
2025-01-02 21:25:11 +0100

    Update partie.c

--- partie.c
+++ partie.c
@@ -38,50 +38,36 @@ void gereTours(Main* J1, Main* J2) {
     char RoVJ1[MAX_ROV], RoVJ2[MAX_ROV];
     int joueurCourant = J1->ordre == 1 ? 1 : 2;
 
-    while (J1->nb != 0 || J2->nb != 0) {
-        if (joueurCourant == 1) {
+    while (J1->nb != 0 || J2->nb != 0)
+        if (joueurCourant == 1)
             jouerTour("1", RoVJ1, mot_J1);
-            if (coupValide(RoVJ1, mot_J1)) {
+            if (coupValide(RoVJ1, mot_J1))
                 jouerTour("2", RoVJ2, mot_J2);
-                if (coupValide(RoVJ2, mot_J2)) {
-                    if (strcmp(RoVJ1, "R") == 0 && strcmp(RoVJ2, "R") == 0) {
+                if (coupValide(RoVJ2, mot_J2))
+                    if (strcmp(RoVJ1, "R") == 0 && strcmp(RoVJ2, "R") == 0)
                         printf("premiere issue\n");
-                    }
-                    else if (strcmp(RoVJ1, "R") == 0 && strcmp(RoVJ2, "V") == 0) {
+                    else if (strcmp(RoVJ1, "R") == 0 && strcmp(RoVJ2, "V") == 0)
                         printf("seconde issue\n");
-                    }
-                    else if (strcmp(RoVJ1, "V") == 0 && strcmp(RoVJ2, "R") == 0) {
+                    else if (strcmp(RoVJ1, "V") == 0 && strcmp(RoVJ2, "R") == 0)
                         printf("troisieme issue\n");
-                    }
-                    else {
+                    else
                         printf("quatrieme issue\n");
-                    }
-                }
-            }
-        }
-        else {
+        else
             jouerTour("2", RoVJ2, mot_J2);
-            if (coupValide(RoVJ2, mot_J2)) {
+            if (coupValide(RoVJ2, mot_J2))
                 jouerTour("1", RoVJ1, mot_J1);
-                if (coupValide(RoVJ1, mot_J1)) {
-                    if (strcmp(RoVJ2, "V") == 0 && strcmp(RoVJ1, "V") == 0) {
+                if (coupValide(RoVJ1, mot_J1))
+                    if (strcmp(RoVJ2, "V") == 0 && strcmp(RoVJ1, "V") == 0)
                         printf("cinquieme issue\n");
-                    }
-                    else if (strcmp(RoVJ2, "V") == 0 && strcmp(RoVJ1, "R") == 0) {
+                    else if (strcmp(RoVJ2, "V") == 0 && strcmp(RoVJ1, "R") == 0)
                         printf("sixieme issue\n");
-                    }
-                    else if (strcmp(RoVJ2, "R") == 0 && strcmp(RoVJ1, "V") == 0) {
+                    else if (strcmp(RoVJ2, "R") == 0 && strcmp(RoVJ1, "V") == 0) 
                         printf("septieme issue\n");
-                    }
-                    else {
+                    else
                         printf("huitieme issue\n");
-                    }
-                }
-            }
-        }
         joueurCourant = 3 - joueurCourant;
-    }
 }
+
 void deroulePartie(Main* J1, Main* J2) {
     assert(J1->ordre != NULL && J2->ordre != NULL);
     gereTours(J1, J2);
