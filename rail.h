c4a19c929ea2f50d3d542ce7aafa7f3bc2ce7031
DanyLLL <dany.lim.91210@gmail.com>
2025-01-03 23:51:55 +0100

    Update rail.h

--- rail.h
+++ rail.h
@@ -1,13 +1,10 @@
 #pragma once
 
-typedef struct {
-	int valeur;
-	struct Maillon* suivant;
-}Maillon;
-
 enum { MAX_RAIL = 8, Last = MAX_RAIL - 1 };
 
 enum { GAUCHE = 0, DROITE = 1 };
+
+enum { MAX_ROV = 2 };
 
 typedef struct {
 	char lettres[MAX_RAIL];
@@ -22,3 +19,5 @@ void ajtRail(Rail* r, const char l, int sens);
 void affRail(Rail* r);
 
 void ajtMotRail(Rail* r, const char* m, int sens);
+
+char* recupMotRail(Rail* rail, int nb, int sens);
