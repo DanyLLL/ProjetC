18e4bf3ae015c2bec3a69c9c62164f7b4d8148b1
DanyLLL <dany.lim.91210@gmail.com>
2025-01-09 01:17:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 #include "partie.h"
 #include "mainj.h"
 #include "rail.h"
+#include "mot.h"
 #pragma warning(disable : 4996)
 
-int main(){
+int main() {
 	Pioche pioche;
 	Main joueur1;
 	Main joueur2;
 	Rail rail;
+	Rail railAnt;
 	initPioche(&pioche);
 	initMain(&joueur1);
 	initMain(&joueur2);
 	initRail(&rail);
-	lancerPartie(&pioche,&joueur1,&joueur2,&rail);
+	initRail(&railAnt);
+	lancerPartie(&pioche, &joueur1, &joueur2, &rail,&railAnt);
 }
