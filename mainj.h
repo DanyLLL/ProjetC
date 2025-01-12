63e39b91b7264ea0a19081a965b5a63b3a57e7ae
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 20:35:45 +0100

    Update mainj.h

--- mainj.h
+++ mainj.h
@@ -9,31 +9,34 @@ typedef Pioche Main;
 
 /**
 * @brief Initialise la main du joueur.
-* @param[in,out] main
+* @param[in,out] main la main à initialiser.
 */
 void initMain(Main* main);
 
 /**
 * @brief Ajoute une lettre à la main du joueur.
-* @param[in,out] main
-* @param[in] lettre
+* @param[in,out] main la main dont on ajoute une lettre.
+* @param[in] lettre la lettre ajoutée.
 */
 void ajouteMain(Main* main, char lettre);
 
 /**
 * @brief Retire la lettre à un indice donné de la main.
-* @param[in,out] main
-* @param[in] indice
-* @return la lettre retiree
+* @param[in,out] main la main dont on retire une lettre.
+* @param[in] indice l'indice auquel on souhaite retirer la lettre.
+* @return la lettre retirée.
 */
 char retireMain(Main* main, int indice);
 
 /**
 * @brief Prépare les deux mains nécessaires pour l'Octo-verso.
-* @param[in,out] pioche
-* @param[in,out] mainJ1
-* @param[in,out] mainJ2
+* @param[in,out] pioche la pioche du jeu.
+* @param[in,out] mainJ1 la main du joueur 1.
+* @param[in,out] mainJ2 la main du joueur 2.
 */
-void prepareMainsJeu(Pioche* pioche,Main* mainJ1,Main* mainJ2);
+void prepareMainsJeu(Pioche* pioche, Main* mainJ1, Main* mainJ2);
 
+/**
+* @brief Teste les fonctions spécifiques du composant mainj.
+*/
 void test_Mainj();
