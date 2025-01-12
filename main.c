7f23530881d4227e45d6aafa419cbfdd214e5db4
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 05:32:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,12 +10,16 @@ int main() {
 	Pioche pioche;
 	Main joueur1;
 	Main joueur2;
+	Main joueur1Ant;
+	Main joueur2Ant;
 	Rail rail;
 	Rail railAnt;
 	initPioche(&pioche);
 	initMain(&joueur1);
 	initMain(&joueur2);
+	initMain(&joueur1Ant);
+	initMain(&joueur2Ant);
 	initRail(&rail);
 	initRail(&railAnt);
-	lancerPartie(&pioche, &joueur1, &joueur2, &rail,&railAnt);
+	lancerPartie(&pioche, &joueur1, &joueur2,&joueur1Ant,&joueur2Ant, &rail,&railAnt);
 }
