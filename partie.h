5838c0c8b76f667e5bc468479c733a675004454b
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 12:17:49 +0100

    Update partie.h

--- partie.h
+++ partie.h
@@ -9,25 +9,34 @@
 void lancerPartie(Pioche* pioche, Main* mainJ1, Main* mainJ2, Main* mainJ1Ant, Main* mainJ2Ant, Rail* rail, Rail* railAnt);
 
 
+
 /**
 * @brief Propose à un joueur de jouer un tour.
 */
 int proposeJouer(const char* joueur, char* RoV, char* mot);
+
+
 
 /**
 * @brief Propose à un joueur de retirer un des chevalets de sa main.
 */
 int proposeRetraitChevalet(Main* mainJ, Pioche* pioche, const char* joueur);
 
+
+
 /**
 * @brief Gère les tours du jeu.
 */
 int gereTours(Main* mainJ1, Main* mainJ2,Main* mainJ1Ant,Main* mainJ2Ant, const char* joueur, Rail* rail, Rail* railAnt, Pioche* pioche);
 
+
+
 /**
 * @brief Déroule la partie après avoir décidé de l'ordre de jeu.
 */
 void deroulePartie(Main* mainJ1, Main* mainJ2, Main* mainJ1Ant, Main* mainJ2Ant, Rail* rail, Rail* railAnt, Pioche* pioche);
+
+
 
 /**
 * @brief Affiche les mains des joueurs et l'état du rail.
