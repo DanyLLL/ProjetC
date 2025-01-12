0073526642c0f4f60a1105a66b33561ecc925f11
yacoub <157528738+yyy219@users.noreply.github.com>
2025-01-12 19:52:31 +0100

    Update mainj.h

--- mainj.h
+++ mainj.h
@@ -2,37 +2,38 @@
 
 #include "pioche.h"
 #include "rail.h"
-
-
 /**
  * @brief Type Main.
  */
 typedef Pioche Main;
 
-
-
 /**
 * @brief Initialise la main du joueur.
+* @param[in,out] main
 */
 void initMain(Main* main);
 
-
-
 /**
 * @brief Ajoute une lettre à la main du joueur.
+* @param[in,out] main
+* @param[in] lettre
 */
 void ajouteMain(Main* main, char lettre);
 
-
-
 /**
 * @brief Retire la lettre à un indice donné de la main.
+* @param[in,out] main
+* @param[in] indice
+* @return la lettre retiree
 */
 char retireMain(Main* main, int indice);
 
-
-
 /**
 * @brief Prépare les deux mains nécessaires pour l'Octo-verso.
+* @param[in,out] pioche
+* @param[in,out] mainJ1
+* @param[in,out] mainJ2
 */
 void prepareMainsJeu(Pioche* pioche,Main* mainJ1,Main* mainJ2);
+
+void test_Mainj();
