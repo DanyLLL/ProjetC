dd252d2fc102920640670ebcdcab423c376334a9
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 12:10:27 +0100

    Update mot.h

--- mot.h
+++ mot.h
@@ -4,6 +4,9 @@
 
 enum { MAX_MOT_DEP = 5, MAX_MOT = 15 ,MAX_HORS_PARENTHESES = 6, MIN_HORS_PARENTHESES = 1,MIN_ENTRE_PARENTHESES = 2, MAX_ENTRE_PARENTHESES = 7,MAX_LIGNE = 256};
 
+/**
+* @brief Vérifie si a est plus proche de A alphabétiquement que b.
+*/
 int triAlphabetique(const void* a, const void* b);
 
 
@@ -13,7 +16,7 @@ int triAlphabetique(const void* a, const void* b);
 int motExiste(const char* mot, const char* dico);
 
 /**
-* @brief Vérifie si le mot est jouable, c'est-à-dire si le joueur a dans sa main les lettres nécessaires pour constituer le mot.
+* @brief Vérifie si le joueur a dans sa main les lettres nécessaires pour constituer le mot et retire les lettres concernées de la main.
 */
 int motJouable(const char* mot, Main* mainJ);
 
@@ -33,7 +36,7 @@ int joueurPlusProcheDeA(const char* motJ1, const char* motJ2);
 char* concateneMotProcheDeA(const char* motJ1, const char* motJ2);
 
 /**
-* @brief 
+* @brief Sépare la partie hors et entre parenthèses du coup joué.
 */
 const char* extraireParenthesesEtMot(const char* chaine, char** entreParenthese, char** horsParenthese);
 /**
@@ -41,7 +44,26 @@ const char* extraireParenthesesEtMot(const char* chaine, char** entreParenthese,
 */
 void deterOrdreJeu(Main* mainJ1, Main* mainJ2, Rail* rail);
 
-char* concatParenthesesEtHors(const char* chaine);
+/**
+* @brief Concatène la partie hors et entre parenthèses du coup joué.
+*/
+char* concatEntreEtHorsParentheses(const char* chaine);
+
+/**
+* @brief Vérifie si le mot est un octo (mot de longueur de 8).
+*/
+int estOcto(const char* mot);
+
+/**
+* @brief Vérifie si le joueur a dans sa main les lettres nécessaires pour constituer le mot sans retirer les lettres concernées de la main.
+*/
+int mainContientMot(const char* mot, Main* main);
+
+/**
+* @brief Vérifie si le coup du joueur est valide aux règles du jeu.
+*/
+int coupValide(const char* RoV, const char* mot);
+
 
 /**
 * @brief Vérifie si un coup est légal.
@@ -49,8 +71,7 @@ char* concatParenthesesEtHors(const char* chaine);
 int coupLegal(Main* mainJCourant, Main* mainJAdverse, Main* mainJCourantAnt, Main* mainJAdverseAnt, Rail* rail, Rail* railAnt, const char* motJ, const char* RoV);
 
 
-int estOcto(const char* mot);
-
-int mainContientMot(const char* mot, Main* main);
-
+/**
+* @brief Vérifie si un signalement est valide en recto.
+*/
 int signalementValide(Pioche* pioche, Main* mainJCourant, Main* mainJAdverse, Rail* railAnt, const char* motJ, const char* joueur);
