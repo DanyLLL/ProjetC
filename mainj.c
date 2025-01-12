3803064ccfb9ad4ff1fd1527c405981123fbb929
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 19:42:17 +0100

    Update mainj.c

--- mainj.c
+++ mainj.c
@@ -7,23 +7,23 @@
 
 void initMain(Main* main) {
 	main->nb = 0;
-	main->chevalets = (char*)malloc(sizeof(char));
+	main->chevalets = (char*)malloc(sizeof(char)); // On alloue la mémoire nécessaire.
 }
 
 void ajouteMain(Main* main, char lettre) {
-	main->chevalets = (char*)realloc(main->chevalets, sizeof(char) * (main->nb + 2));
+	main->chevalets = (char*)realloc(main->chevalets, sizeof(char) * (main->nb + 2)); // On realloue la mémoire.
 	main->chevalets[main->nb] = lettre; // Ajoute la lettre
 	main->nb++;
-	main->chevalets[main->nb] = '\0'; // Ajoute '\0'
+	main->chevalets[main->nb] = '\0'; 
 }
 
 char retireMain(Main* main, int indice) {
 	char lettre = main->chevalets[indice];
 	for (int i = indice; i < main->nb - 1; i++) {
-		main->chevalets[i] = main->chevalets[i + 1];
+		main->chevalets[i] = main->chevalets[i + 1]; // On décale tous les chevalets afin d'écraser celui à l'indice donné.
 	}
 	main->nb = main->nb - 1;
-	char* test_allocation = (char*)realloc(main->chevalets, sizeof(char) * (main->nb + 2));
+	char* test_allocation = (char*)realloc(main->chevalets, sizeof(char) * (main->nb + 2)); // On realloue la mémoire.
 	if (test_allocation) {
 		main->chevalets = test_allocation;
 	}
@@ -70,7 +70,7 @@ void prepareMainsJeu(Pioche* pioche, Main* mainJ1, Main* mainJ2) {
 			i = i + 1;
 		}
 	}*/
-	qsort(mainJ1->chevalets, strlen(mainJ1->chevalets), sizeof(char), triAlphabetique);
+	qsort(mainJ1->chevalets, strlen(mainJ1->chevalets), sizeof(char), triAlphabetique); // On trie avant l'affichage.
 	qsort(mainJ2->chevalets, strlen(mainJ2->chevalets), sizeof(char), triAlphabetique);
 	printf("1 : %s\n", mainJ1->chevalets);
 	printf("2 : %s\n\n", mainJ2->chevalets);
