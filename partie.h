37dd9e88490796ee94022dc77b35fb20fc4b14be
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 20:42:00 +0100

    Update partie.h

--- partie.h
+++ partie.h
@@ -3,66 +3,65 @@
 #include "mot.h"
 /**
 * @brief Lance toutes les fonctions permettant de lancer la partie.
-* @param[in,out] pioche
-* @param[in,out] mainJ1
-* @param[in,out] mainJ2
-* @param[in,out] mainJ1Ant
-* @param[in,out] mainJ2Ant
-* @param[in,out] rail
-* @param[in,out] railAnt
+* @param[in,out] pioche la pioche de jeu.
+* @param[in,out] mainJ1 la main du joueur 1.
+* @param[in,out] mainJ2 la main du joueur 2.
+* @param[in,out] mainJ1Ant la main du joueur 1 au tour précédent.
+* @param[in,out] mainJ2Ant la main du joueur 2 au tour précédent.
+* @param[in,out] rail le rail de jeu.
+* @param[in,out] railAnt le rail de jeu au tour précédent.
 */
 void lancerPartie(Pioche* pioche, Main* mainJ1, Main* mainJ2, Main* mainJ1Ant, Main* mainJ2Ant, Rail* rail, Rail* railAnt);
 
 
 /**
 * @brief Propose à un joueur de jouer un tour.
-* @param[in] joueur
+* @param[in] joueur le joueur dont c'est le tour.
 * @param[in] RoV le premier caractere du coup
-* @param[in] mot
-* @return 0 si le coup est valide 1 si non
+* @param[in] mot le mot proposé.
+* @return 0 si le coup est valide 1 sinon
 */
 int proposeJouer(const char* joueur, char* RoV, char* mot);
 
 /**
 * @brief Propose à un joueur de retirer un des chevalets de sa main.
-* @param[in,out] pioche
-* @param[in,out] mainJ
+* @param[in,out] pioche la pioche de jeu.
+* @param[in,out] mainJ la main du joueur qui retire un chevalet de sa main.
 * @param[in] joueur 1 ou 2
-* return 0 si le chevalet est retirable 1 si non
+* return 0 si le chevalet a été retiré correctement 1 sinon
 */
 int proposeRetraitChevalet(Main* mainJ, Pioche* pioche, const char* joueur);
 
 /**
 * @brief Gère les tours du jeu.
-* @param[in,out] mainJ1
-* @param[in,out] mainJ2
-* @param[in] mainJ1Ant
-* @param[in] mainJ2Ant
-* @param[in] joueur
-* @param[in,out] rail
-* @param[in] railAnt
-* @param[in,out] pioche
-* @return 0 si le tour se passe bien 1 si non
+* @param[in,out] mainJ1 la main du joueur 1.
+* @param[in,out] mainJ2 la main du joueur 2.
+* @param[in] mainJ1Ant la main du joueur 1 au tour précédent.
+* @param[in] mainJ2Ant la main du joueur 2 au tour précédent.
+* @param[in] joueur le joueur dont c'est le tour.
+* @param[in,out] rail le rail de jeu.
+* @param[in] railAnt le rail de jeu au tour précédent.
+* @param[in,out] pioche la pioche de jeu.
+* @return 0 si le tour se passe bien 1 sinon
 */
-int gereTours(Main* mainJ1, Main* mainJ2,Main* mainJ1Ant,Main* mainJ2Ant, const char* joueur, Rail* rail, Rail* railAnt, Pioche* pioche);
+int gereTours(Main* mainJ1, Main* mainJ2, Main* mainJ1Ant, Main* mainJ2Ant, const char* joueur, Rail* rail, Rail* railAnt, Pioche* pioche);
 
 /**
 * @brief Déroule la partie après avoir décidé de l'ordre de jeu.
-* @param[in,out] mainJ1
-* @param[in,out] mainJ2
-* @param[in] mainJ1Ant
-* @param[in] mainJ2Ant
-* @param[in] joueur
-* @param[in,out] rail
-* @param[in] railAnt
-* @param[in,out] pioche
+* @param[in,out] mainJ1 la main du joueur 1.
+* @param[in,out] mainJ2 la main du joueur 2.
+* @param[in] mainJ1Ant la main du joueur 1 au tour précédent.
+* @param[in] mainJ2Ant la main du joueur 2 au tour précédent.
+* @param[in,out] rail le rail de jeu.
+* @param[in] railAnt le rail de jeu au tour précédent.
+* @param[in,out] pioche la pioche de jeu.
 */
 void deroulePartie(Main* mainJ1, Main* mainJ2, Main* mainJ1Ant, Main* mainJ2Ant, Rail* rail, Rail* railAnt, Pioche* pioche);
 
 /**
 * @brief Affiche les mains des joueurs et l'état du rail.
-* @param[in] mainJ1
-* @param[in] mainJ2
-* @param[in] rail
+* @param[in] mainJ1 la main du joueur 1.
+* @param[in] mainJ2 la main du joueur 2.
+* @param[in] rail le rail de jeu.
 */
 void afficheSituationCouranteJeu(Main* mainJ1, Main* mainJ2, Rail* rail);
