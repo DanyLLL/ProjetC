6f765f11632f1e0624e87fd0d75ed5c137ebb741
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 12:18:17 +0100

    Update pioche.h

--- pioche.h
+++ pioche.h
@@ -7,7 +7,11 @@ enum {
 	NB_CHEVALET = 88
 };
 
+
+
 enum {NB_A = 9,NB_C = 2,NB_D = 3,NB_E = 14,NB_I = 7,NB_L = 5, NB_M = 3, NB_N = 6, NB_O = 5, NB_P = 2,NB_R = 6,NB_S = 7,NB_T = 6,NB_U = 5,NB_V = 2};
+
+
 
 /**
  * @brief Type Pioche .
@@ -18,20 +22,28 @@ typedef struct {
 	char* chevalets; /// < Chevalets dans la pioche .
 } Pioche;
 
+
+
 /**
 * @brief Initialise la pioche.
 */
 void initPioche(Pioche* pioche);
+
+
 
 /**
 * @brief Ajoute une lettre à la pioche.
 */
 void ajoutePioche(Pioche* pioche, char lettre);
 
+
+
 /**
 * @brief Retire la lettre à un indice donné de la pioche.
 */
 char retirePioche(Pioche* pioche,int indice);
+
+
 
 /**
 * @brief Prépare les différentes lettres nécessaires pour l'Octo-verso.
