f0f96860f89427f8480e4fe1c75ed46a72639a9a
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 05:32:18 +0100

    Update pioche.c

--- pioche.c
+++ pioche.c
@@ -34,56 +34,59 @@ char retirePioche(Pioche* pioche, int indice) {
 
 
 void preparePiocheJeu(Pioche* pioche) {
-	assert(pioche->nb >= 0);
-	for (int i = 0; i < 9; i = i + 1) {
+	assert(pioche->nb >= 0); 
+
+	// On remplit la pioche en fonction du paquet défini (cf sujet)
+	
+	for (int i = 0; i < NB_A; i = i + 1) {
 		ajoutePioche(pioche, 'A');
 	}
 	ajoutePioche(pioche, 'B');
-	for (int i = 0; i < 2; i = i + 1) {
+	for (int i = 0; i < NB_C; i = i + 1) {
 		ajoutePioche(pioche, 'C');
 	}
-	for (int i = 0; i < 3; i = i + 1) {
+	for (int i = 0; i < NB_D; i = i + 1) {
 		ajoutePioche(pioche, 'D');
 	}
-	for (int i = 0; i < 14; i = i + 1) {
+	for (int i = 0; i < NB_E; i = i + 1) {
 		ajoutePioche(pioche, 'E');
 	}
 	ajoutePioche(pioche, 'F');
 	ajoutePioche(pioche, 'G');
 	ajoutePioche(pioche, 'H');
-	for (int i = 0; i < 7; i = i + 1) {
+	for (int i = 0; i < NB_I; i = i + 1) {
 		ajoutePioche(pioche, 'I');
 	}
 	ajoutePioche(pioche, 'J');
-	for (int i = 0; i < 5; i = i + 1) {
+	for (int i = 0; i < NB_L; i = i + 1) {
 		ajoutePioche(pioche, 'L');
 	}
-	for (int i = 0; i < 3; i = i + 1) {
+	for (int i = 0; i < NB_M; i = i + 1) {
 		ajoutePioche(pioche, 'M');
 	}
-	for (int i = 0; i < 6; i = i + 1) {
+	for (int i = 0; i < NB_N; i = i + 1) {
 		ajoutePioche(pioche, 'N');
 	}
-	for (int i = 0; i < 5; i = i + 1) {
+	for (int i = 0; i < NB_O; i = i + 1) {
 		ajoutePioche(pioche, 'O');
 	}
-	for (int i = 0; i < 2; i = i + 1) {
+	for (int i = 0; i < NB_P; i = i + 1) {
 		ajoutePioche(pioche, 'P');
 	}
 	ajoutePioche(pioche, 'Q');
-	for (int i = 0; i < 6; i = i + 1) {
+	for (int i = 0; i < NB_R; i = i + 1) {
 		ajoutePioche(pioche, 'R');
 	}
-	for (int i = 0; i < 7; i = i + 1) {
+	for (int i = 0; i < NB_S; i = i + 1) {
 		ajoutePioche(pioche, 'S');
 	}
-	for (int i = 0; i < 6; i = i + 1) {
+	for (int i = 0; i < NB_T; i = i + 1) {
 		ajoutePioche(pioche, 'T');
 	}
-	for (int i = 0; i < 5; i = i + 1) {
+	for (int i = 0; i < NB_U; i = i + 1) {
 		ajoutePioche(pioche, 'U');
 	}
-	for (int i = 0; i < 2; i = i + 1) {
+	for (int i = 0; i < NB_V; i = i + 1) {
 		ajoutePioche(pioche, 'V');
 	}
 }
