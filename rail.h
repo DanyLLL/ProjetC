280daf36e2a4dc044749db5cdba7a9ee6220135e
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 12:17:31 +0100

    Update rail.h

--- rail.h
+++ rail.h
@@ -12,14 +12,40 @@ typedef struct {
 	int fin;
 }Rail;
 
+
+
+/**
+* @brief Initialise le rail.
+*/
 int initRail(Rail* r);
 
+
+
+/**
+* @brief Ajoute une lettre au rail en fonction du sens.
+*/
 char ajouteRail(Rail* r, const char l, int sens);
 
+
+
+/**
+* @brief Affiche le recto et le verso du rail.
+*/
 void afficherRail(Rail* r);
 
+
+
+/**
+* @brief Ajoute un mot au rail en fonction du sens.
+*/
 char* ajouteMotRail(Rail* r, const char* m, int sens);
 
+/**
+* @brief Récupère nb lettres à l'extrémité du rail en fonction du sens.
+*/
 char* recupMotRail(Rail* rail, int nb, int sens);
 
+/**
+* @brief Inverse le rail.
+*/
 Rail inverseRail(const Rail* rail);
