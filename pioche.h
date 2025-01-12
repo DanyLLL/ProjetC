8a932e145f999cc8e63eb2f31c526aa8782d1e04
yacoub <157528738+yyy219@users.noreply.github.com>
2025-01-12 19:54:04 +0100

    Update pioche.h

--- pioche.h
+++ pioche.h
@@ -8,10 +8,7 @@ enum {
 };
 
 
-
 enum {NB_A = 9,NB_C = 2,NB_D = 3,NB_E = 14,NB_I = 7,NB_L = 5, NB_M = 3, NB_N = 6, NB_O = 5, NB_P = 2,NB_R = 6,NB_S = 7,NB_T = 6,NB_U = 5,NB_V = 2};
-
-
 
 /**
  * @brief Type Pioche .
@@ -22,30 +19,32 @@ typedef struct {
 	char* chevalets; /// < Chevalets dans la pioche .
 } Pioche;
 
-
-
 /**
 * @brief Initialise la pioche.
+* @param[in,out] pioche
 */
 void initPioche(Pioche* pioche);
 
-
-
 /**
 * @brief Ajoute une lettre à la pioche.
+* @param[in,out] pioche
+* @param[in] lettre
 */
 void ajoutePioche(Pioche* pioche, char lettre);
 
-
-
 /**
 * @brief Retire la lettre à un indice donné de la pioche.
+* @param[in,out] pioche
+* @param[in] indice
+* @return la lettre retiree
 */
 char retirePioche(Pioche* pioche,int indice);
 
-
-
 /**
 * @brief Prépare les différentes lettres nécessaires pour l'Octo-verso.
+* @param[in,out] pioche
 */
 void preparePiocheJeu(Pioche* pioche);
+
+
+void test_pioche();
