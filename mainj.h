ce7a65e32cd2a1a6944fa2bf159941ec96141826
DanyLLL <dany.lim.91210@gmail.com>
2025-01-12 12:18:36 +0100

    Update mainj.h

--- mainj.h
+++ mainj.h
@@ -2,25 +2,35 @@
 
 #include "pioche.h"
 
+
+
 /**
  * @brief Type Main.
  */
 typedef Pioche Main;
+
+
 
 /**
 * @brief Initialise la main du joueur.
 */
 void initMain(Main* main);
 
+
+
 /**
 * @brief Ajoute une lettre à la main du joueur.
 */
 void ajouteMain(Main* main, char lettre);
 
+
+
 /**
 * @brief Retire la lettre à un indice donné de la main.
 */
 char retireMain(Main* main, int indice);
+
+
 
 /**
 * @brief Prépare les deux mains nécessaires pour l'Octo-verso.
